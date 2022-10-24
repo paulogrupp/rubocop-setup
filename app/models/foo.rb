a3dad4627f45125fde85ea1756a57cec86ec4b26
paulogrupp <paulo.rupp198@gmail.com>
2022-10-24 18:02:12 -0300

    even dirtier class

--- app/models/foo.rb
+++ app/models/foo.rb
@@ -2,4 +2,29 @@ class Foo
   def myMethod
     return [:key1, :key2, :key3]
   end
+def myMethod
+end
+
+  def conditions
+    key = myMethod.sample
+    if key == :key1
+      puts key
+    elsif key == :key2
+      puts key
+    elsif key == :key3
+      puts key
+    elsif key == :key4
+      puts key
+    elsif key == :key5
+      puts key
+    elsif key == :key6
+      puts key
+    elsif key == :key7
+      puts key
+    elsif key == :key8
+      puts key
+    elsif key == :key9
+      eval(key)
+    end
+  end
 end
